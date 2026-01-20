9025edfa8560bde26616d77529fc27d5522c414c
Mahesh <maheshkarnati405@gamil.com>
2026-01-20 19:38:08 +0530

    Added JavaScript assignments

---
+++ task4.js
@@ -0,0 +1,15 @@
+//object(pack of properties)
+//iterate for-in
+//create student onject with props rollno,name,age,city
+//create product obj with props productname ,brand,price
+let student={
+    rollno:25,
+    name:'mahesh'
+    ,age:24,city:'hyd'
+
+}
+let product={
+    productname:'watch',
+    brand:'rolex',
+    price:'2500'
+}
